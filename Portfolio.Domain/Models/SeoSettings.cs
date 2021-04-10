d230912d58b43a1da8d85d68cdf7dc0158dfcfc7
Bart Blokhuis <info@bartblokhuis.com>
2021-04-10 05:05:06 +0200

    Created model and migration

---
+++ Portfolio.Domain/Models/SeoSettings.cs
@@ -0,0 +1,17 @@
+﻿using Portfolio.Domain.Models.Common;
+
+namespace Portfolio.Domain.Models
+{
+    public class SeoSettings : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string DefaultMetaKeywords { get; set; }
+
+        public string DefaultMetaDescription { get; set; }
+
+        public bool UseTwitterMetaTags { get; set; }
+
+        public bool UseOpenGraphMetaTags { get; set; }
+    }
+}
---
+++ Portfolio.Migrations/Migrations/20210410030431_seo_settings_init.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Portfolio.Migrations.Migrations
+{
+    public partial class seo_settings_init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SeoSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DefaultMetaKeywords = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DefaultMetaDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UseTwitterMetaTags = table.Column<bool>(type: "bit", nullable: false),
+                    UseOpenGraphMetaTags = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeoSettings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SeoSettings");
+        }
+    }
+}
